d8a08d5de7ab935b1b2119ff8e87b97edbe20117
Fernando de la Cruz Soto <fdelacruzsoto@gmail.com>
2018-03-21 20:36:00 -0600

    Display the list of existing posts
    Added a few packages

--- front-end-another-blog-in-the-wall/src/App.js
+++ front-end-another-blog-in-the-wall/src/App.js
@@ -1,18 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.min.css';
+import {
+  Navbar,
+  NavbarBrand,
+  Alert,
+  ListGroup,
+  ListGroupItem,
+  Button,
+} from 'reactstrap';
+
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    posts: [],
+    error: false
+  }
+
+  async componentDidMount() {
+    try {
+      const {data} = await axios.get("http://localhost:3333/post");
+      const {posts} = data;
+      this.setState({posts: posts});
+    } catch (e) {
+      this.setState({error: true});
+    }
+  }
+
   render() {
+
+    const {posts, error} = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navbar
+          className="navbar-main"
+          color="dark"
+          dark
+          fixed="top">
+          <NavbarBrand>Another blog in the wall</NavbarBrand>
+        </Navbar>
+        <div id="body">
+          {error &&
+          <Alert color="danger">
+            Error getting posts from server
+          </Alert>}
+          {!error &&
+          <ListGroup>
+            <ListGroupItem key="add">
+              <Button color="primary">Add new post</Button>
+            </ListGroupItem>
+            {posts.length && posts.map((post, i) => <ListGroupItem key={i}>{post.title} <i class="fa fa-edit"></i></ListGroupItem>)}
+          </ListGroup>}
+        </div>
       </div>
     );
   }
